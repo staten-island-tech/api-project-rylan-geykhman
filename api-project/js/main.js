4068380e79d61bd871247cd8962a67db3590eb74
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-12-11 13:14:09 -0500

    some work

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,3 +1,7 @@
+const DOMSelectors = {
+  button: document.querySelector(".button"),
+}
+
 async function getData() {
   let res = await fetch(
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
@@ -6,3 +10,23 @@ async function getData() {
   console.log(data);
 }
 getData();
+
+//Get the currency list with USD as base currency:
+//https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json
+
+DOMSelectors.button.addEventListener("click", async function (event){
+  event.preventDefault()
+  let res = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json"
+  );
+  let data = await res.json();
+  console.log(data)
+})
+
+//function compare (x,y){}
+//write function that converts both currencies into USD, then compares which one is worth more.
+
+//utilize function compare after someone guesses if currency x is higher or lower than currency y.
+//if person selects higher,
+//if person is correct, then display "Correct!", then the values of both
+//else, display "Womp Womp", then the values of both
