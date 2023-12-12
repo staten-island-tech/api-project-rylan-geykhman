68403c53db3749c79fbd44713085d40586e02323
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-12-12 13:15:10 -0500

    commit

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,10 +1,21 @@
 const DOMSelectors = {
   button: document.querySelector(".button"),
+  container: document.querySelector(".container"),
 }
 
 async function getData() {
-  let res = await fetch(
+  let theCurrency = await fetch(
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+  )
+
+  let datav = await theCurrency.json();
+  const rylena = []
+  datav.forEach((something)=>rylena.push(something))
+  console.log(rylena)
+  
+  let currency = rylena.random()
+  let res = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
   );
   let data = await res.json();
   console.log(data);
@@ -17,16 +28,17 @@ getData();
 DOMSelectors.button.addEventListener("click", async function (event){
   event.preventDefault()
   let res = await fetch(
-    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/usd.json"
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
   );
   let data = await res.json();
   console.log(data)
 })
 
-//function compare (x,y){}
+//function compare(x,y){}
 //write function that converts both currencies into USD, then compares which one is worth more.
 
 //utilize function compare after someone guesses if currency x is higher or lower than currency y.
-//if person selects higher,
-//if person is correct, then display "Correct!", then the values of both
-//else, display "Womp Womp", then the values of both
+//if person selects higher, check if x > y. if x > y, they are correct! if x < y, they are wrong
+//if person selects lower, check if x < y. if x < y, they are correct! if x > y, they are wrong
+//if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one.
+//if person is wrong, display "Womp Womp", then the values of both currencies. show score, then give a button to replay. when replaying, reset score.
