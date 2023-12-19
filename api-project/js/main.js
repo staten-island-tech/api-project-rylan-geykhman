a387c0a3cefa9e43eac88920f89b94476364fc08
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-12-19 13:12:36 -0500

    update

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -3,6 +3,8 @@ const DOMSelectors = {
   container: document.querySelector(".container"),
 }
 
+const theTrueArray = []
+
 async function getRandom() {
   let theCurrency = await fetch(
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
@@ -18,17 +20,16 @@ async function getRandom() {
   );
   let data = await res.json();
   console.log(data);
+  theTrueArray.push(data);
+  console.log(theTrueArray);
 }
-getRandom();
-getRandom();
+
+getRandom()
+getRandom()
 
 DOMSelectors.button.addEventListener("click", async function (event){
   event.preventDefault()
-  let res = await fetch(
-    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json"
-  );
-  let data = await res.json();
-  console.log(data)
+  getRandom();
 })
 
 function compare(x,y){
@@ -39,5 +40,5 @@ function compare(x,y){
 //utilize function compare after someone guesses if currency x is higher or lower than currency y.
 //if person selects higher, check if x > y. if x > y, they are correct! if x < y, they are wrong
 //if person selects lower, check if x < y. if x < y, they are correct! if x > y, they are wrong
-//if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one.
+//if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one. Splice to remove the first one, and generate a new one
 //if person is wrong, display "Womp Womp", then the values of both currencies. show score, then give a button to replay. when replaying, reset score.
