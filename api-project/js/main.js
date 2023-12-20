baa04007ce80e68d462872cae75f779971922909
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2023-12-20 13:13:27 -0500

    commit

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,44 +1,50 @@
-const DOMSelectors = {
-  button: document.querySelector(".button"),
-  container: document.querySelector(".container"),
-}
-
-const theTrueArray = []
-
-async function getRandom() {
-  let theCurrency = await fetch(
-    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
-  )
-  let theData = await theCurrency.json();
-  const currencyValues = []
-  for (const currencies in theData) {
-    currencyValues.push(currencies)
+  const DOMSelectors = {
+    button: document.querySelector(".button"),
+    container: document.querySelector(".container"),
   }
-  let currency = Math.floor(Math.random() * currencyValues.length);
-  let res = await fetch(
-    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyValues[currency]}.json`
-  );
-  let data = await res.json();
-  console.log(data);
-  theTrueArray.push(data);
-  console.log(theTrueArray);
-}
 
-getRandom()
-getRandom()
+  const theTrueArray = []
 
-DOMSelectors.button.addEventListener("click", async function (event){
-  event.preventDefault()
-  getRandom();
-})
+  async function getRandom() {
+    let theCurrency = await fetch(
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+    )
+    let theData = await theCurrency.json();
+    const currencyValues = []
+    for (const currencies in theData) {
+      currencyValues.push(currencies)
+    }
+    let currency = Math.floor(Math.random() * currencyValues.length);
+    let res = await fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyValues[currency]}.json`
+    );
+    let data = await res.json();
+    theTrueArray.push(data);
+    console.log(data[`${currencyValues[currency]}`]);
+    console.log(theTrueArray)
+    DOMSelectors.button.addEventListener("click", async function (event){
+      event.preventDefault()
+      console.log('test',data)
+      compare(data,data);
+    })
+  }
 
-function compare(x,y){
-  if (x.getRandom() > y.getRandom()){}
-}
-//write function that converts both currencies into USD, then compares which one is worth more.
+  getRandom()
+  getRandom()
 
-//utilize function compare after someone guesses if currency x is higher or lower than currency y.
-//if person selects higher, check if x > y. if x > y, they are correct! if x < y, they are wrong
-//if person selects lower, check if x < y. if x < y, they are correct! if x > y, they are wrong
-//if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one. Splice to remove the first one, and generate a new one
-//if person is wrong, display "Womp Womp", then the values of both currencies. show score, then give a button to replay. when replaying, reset score.
+
+
+  function compare(x,y){
+    if (x.usd > y.usd){
+      console.log("yippee!")
+    } else {
+      console.log("nay")
+    }
+  }
+  //write function that converts both currencies into USD, then compares which one is worth more.
+
+  //utilize function compare after someone guesses if currency x is higher or lower than currency y.
+  //if person selects higher, check if x > y. if x > y, they are correct! if x < y, they are wrong
+  //if person selects lower, check if x < y. if x < y, they are correct! if x > y, they are wrong
+  //if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one. Splice to remove the first one, and generate a new one
+  //if person is wrong, display "Womp Womp", then the values of both currencies. show score, then give a button to replay. when replaying, reset score.
---
+++ api-project/js/tempCodeRunnerFile.js
@@ -0,0 +1,49 @@
+const DOMSelectors = {
+  button: document.querySelector(".button"),
+  container: document.querySelector(".container"),
+}
+
+const theTrueArray = []
+
+async function getRandom() {
+  let theCurrency = await fetch(
+    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+  )
+  let theData = await theCurrency.json();
+  const currencyValues = []
+  for (const currencies in theData) {
+    currencyValues.push(currencies)
+  }
+  let currency = Math.floor(Math.random() * currencyValues.length);
+  let res = await fetch(
+    `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyValues[currency]}.json`
+  );
+  let data = await res.json();
+  console.log(data[currency]);
+  theTrueArray.push(data);
+  console.log(theTrueArray);
+}
+
+getRandom()
+getRandom()
+
+DOMSelectors.button.addEventListener("click", async function (event){
+  event.preventDefault()
+  compare();
+})
+
+function compare(x,y){
+  if (x.theTrueArray.usd > y.theTrueArray.usd){
+    console.log("yippee!")
+  }
+  else {
+    console.log("nay")
+  }
+}
+//write function that converts both currencies into USD, then compares which one is worth more.
+
+//utilize function compare after someone guesses if currency x is higher or lower than currency y.
+//if person selects higher, check if x > y. if x > y, they are correct! if x < y, they are wrong
+//if person selects lower, check if x < y. if x < y, they are correct! if x > y, they are wrong
+//if person is correct, then display "Correct!", then the values of both currencies. add 1 to the counter, and give a button to go to the next one. Splice to remove the first one, and generate a new one
+//if person is wrong, display "Womp Womp", then the values of both currencies. show score, then give a button to replay. when replaying, reset score.
